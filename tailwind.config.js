ab5014bd02fa558eb117c6b6dc808ae1f7a9de69
NicoSelomin <vodounnounicodemeselomin@gmail.com>
2024-06-14 21:16:40 +0100

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,41 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*.{html,js}"],
+  theme: {
+    container :{
+      padding : {
+        DEFAULT : '15px',
+      },
+    },
+    screens : {
+      sm : '640px',
+      md : '768px',
+      lg : '960px',
+      sm : '1200px',
+    },
+    fontFamily : {
+      primary : 'DM Serif Display',
+      secondary : 'Jost',
+    },
+    backgroundImage : {
+      hero : 'url(/assets/hero/bg.jpg)',
+      grid : 'url(/assets/grid.png)',      
+    },
+    extend: {
+      colors :{
+        primary: {
+          DEFAULT : '#292f36',
+          hover : '#343e4e',
+        },
+        secondary : '#4d5053',
+        accent : {
+          DEFAULT : '#cda274',
+          secondary : '#f4fGec',
+          hover : '#b88c5d ',
+        },
+
+      }
+    },
+  },
+  plugins: [],
+}
