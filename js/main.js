5f34dc40761701931f6b9ca85d6b34576af46bcd
NicoSelomin <vodounnounicodemeselomin@gmail.com>
2024-06-15 23:06:03 +0100

    testimonial section added

--- js/main.js
+++ js/main.js
@@ -3,3 +3,28 @@ const navMenu = document.querySelector("#nav_menu");
 navTriggerBtn.addEventListener("click", () => {
     navMenu.classList.toggle('nav-is-open');
 });
+
+// swiper
+const swiper = new Swiper('.swiper', {
+    loop: true,
+    pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+    },
+    // braekpoints
+    slidesPerView: 3,
+    spaceBetween: 20,
+    breakpoints: {
+        320:{
+            slidesPerView: 1,
+        },
+        960:{
+            slidesPerView: 2,
+        },
+        1200:{
+            slidesPerView: 3,
+        }
+
+    }
+
+});
