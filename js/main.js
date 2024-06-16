1d49484e1be8e8ff63a2a0a65ce1c08bc281aa01
NicoSelomin <vodounnounicodemeselomin@gmail.com>
2024-06-17 00:50:40 +0100

    animation added the site

--- js/main.js
+++ js/main.js
@@ -28,3 +28,70 @@ const swiper = new Swiper('.swiper', {
     }
 
 });
+
+
+const sr = ScrollReveal({
+    origin:'bottom',
+    distance:'60px',
+    duration:3000,
+    //reset:true
+});
+
+
+//hero
+sr.reveal('.hero__text', {origin:'top'});
+
+//steps
+sr.reveal('.steps__step', {distance:'100px', interval: 100});
+
+//about
+sr.reveal('.about__text', {origin:'left'});
+sr.reveal('.about__img', {origin:'right', delay: 800});
+
+//testimonial
+sr.reveal('.testimonial__bg', {delay: 800});
+sr.reveal('.testimonial__title');
+sr.reveal('.testimonial__slider', {delay: 1000});
+
+//brands
+sr.reveal('.brands__img', {delay: 600, distance: '100px', interval: 100});
+
+//work
+sr.reveal('.work__title');
+sr.reveal('.work__subtitle',{delay: 800});
+sr.reveal('.work__grid',{delay: 1000});
+
+//stats
+sr.reveal('.stats');
+sr.reveal('.stats_item', {
+    distance: '100px', 
+    interval: 100
+});
+
+//news
+sr.reveal('.news__title');
+sr.reveal('.news__subtitle',{delay: 800});
+sr.reveal('.news-item', {
+    distance: '100px', 
+    interval: 100,
+    delay: 1000
+});
+
+//contact
+sr.reveal('.contact__container');
+sr.reveal('.contact__text',{delay: 800});
+
+//footer
+sr.reveal('.footer__item', {
+    distance: '100px', 
+    interval: 100,
+});
+
+sr.reveal('.footer__copyright');
+
+ 
+
+
+
+
+
